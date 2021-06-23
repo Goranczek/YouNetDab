fdbb08f735a269716b9d46f32ca3a09cc4ea6479
Goranczek <michalek.picek@seznam.cz>
2021-06-24 00:03:45 +0200

    Updating gui

--- gui.py
+++ gui.py
@@ -22,10 +22,22 @@ class Window(object):
         l1 = Label(root, text="files")
         l1.grid(row=0, column=0)
 
+        login_holder = StringVar()
+        self.login = Label(self.root, text="Username")
+        self.login.grid(row=0, column=0)
+        self.textfield_login = ttk.Entry(self.root, textvariable=login_holder)
+        self.textfield_login.grid(row=0, column=1)
+
+        pass_holder = StringVar()
+        self.password = Label(self.root, text="Password")
+        self.password.grid(row=1, column=0)
+        self.textfield_pass = ttk.Entry(self.root, textvariable=pass_holder)
+        self.textfield_pass.config(show="*")
+        self.textfield_pass.grid(row=1, column=1)
         # root combobox
         loader_tmp = StringVar()
         self.cb1 = ttk.Combobox(root, textvariable=loader_tmp)
-        self.cb1.grid(row=0, column=1)
+        self.cb1.grid(row=2, column=1)
 
         script_holder = StringVar()
         self.cmb2 = ttk.Combobox(self.root, textvariable=script_holder)
@@ -70,32 +82,29 @@ class Window(object):
                 list_files.append(file.name)
         self.loader(list_files)
         btn = ttk.Button(self.root, text="Get scripts", command=self.get_scripts)
-        btn.grid(row=0, column=2)
+        btn.grid(row=2, column=2)
 
     def get_scripts(self):
         file = self.cb1.get()
         self.full_dict[file] = {}
         l2 = Label(self.root, text="Choose script")
-        l2.grid(row=0, column=3)
-        self.cmb2.grid(row=0, column=4)
+        l2.grid(row=2, column=3)
+        self.cmb2.grid(row=2, column=4)
         self.cmb2["values"] = self.scripts_parser(file)
         self.show_entry()
         btn2 = Button(self.root, text="Approve", command=self.set_dict)
-        btn2.grid(row=0, column=5)
+        btn2.grid(row=2, column=5)
 
     def show_entry(self):
-        self.l3.grid(row=1, column=3)
+        self.l3.grid(row=3, column=3)
         self.textfield_prod_tables.grid(row=1, column=4)
-        self.l4.grid(row=2, column=3)
+        self.l4.grid(row=4, column=3)
         self.textfield_dev_tables.grid(row=2, column=4)
 
     def set_dict(self):
-        print(self.full_dict)
         file = self.cb1.get()
-        print(self.full_dict)
         script = self.cmb2.get()
         self.full_dict[file][script] = {}
-        print(self.full_dict)
         prod_tables = self.textfield_prod_tables.get()
         self.full_dict[file][script]["prod_tables"] = prod_tables
         dev_tables = self.textfield_dev_tables.get()
